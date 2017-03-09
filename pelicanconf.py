e2a4bb3f490a5d2a40d672307418ccac941e30f7
Federico T <fdtomasi@gmail.com>
2017-03-09 22:55:04 +0100

    style

--- pelicanconf.py
+++ pelicanconf.py
@@ -10,7 +10,7 @@ SITESUBTITLE = "[Personal Web Page]"
 PATH = 'content'
 
 TIMEZONE = 'Europe/Rome'
-
+LOCALE = ('gb', 'en_GB', 'usa', 'en_US', 'en_GB.utf8')
 DEFAULT_LANG = u'en'
 
 # Feed generation is usually not desired when developing
